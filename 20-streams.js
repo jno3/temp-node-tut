c24ec97f4cdb5613cf4b21cdfb432c9e692a1efc
j <joaoribeiro3891@gmail.com>
2022-08-11 11:01:02 -0300

    test

--- 20-streams.js
+++ 20-streams.js
@@ -1,1 +1,14 @@
 const {writeFileSync} = require('fs')
+// for (let i=0; i<10000; i++){
+//     writeFileSync('./content/big.txt', `hello world ${i}\n`, {flag: 'a'})
+// }
+
+const {createReadStream} = require('fs')
+
+const stream = createReadStream('./content/big.txt',
+{highWaterMark: 90000, encoding: 'utf-8'})
+
+stream.on('data', (result) => {
+    console.log(result)
+})
+stream.on('error', (err) => console.log(err))
--- extra.js
+++ extra.js
@@ -2,10 +2,18 @@ function greeting(name) {
     console.log('Hello ' + name);
   }
   
-  function processUserInput(callback) {
+  function processUserInput(callback, callback2) {
     var name = 'pexe'
+    callback2({
+      name: 'vivivi',
+      surname: 'ahjaha',
+      gugug: 'pexe'
+    })
     callback(name);
   }
   
-  processUserInput(greeting);
+  processUserInput(greeting, (callback2) =>{
+    console.log(callback2.name)
+  });
   
+
